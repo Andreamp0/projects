5032d71afe5c195bf91d04856287d850aa3233ee
Andreamp0 <112758957+Andreamp0@users.noreply.github.com>
2025-01-04 23:47:36 +0100

    Update sketch.js

--- asteroid/sketch.js
+++ asteroid/sketch.js
@@ -1,114 +1,209 @@
 function setup() {
   createCanvas(400, 400);
-  var rock = loadImage("Rock.png");
-var rocks = [];
-var Rock = function(x, y, size, distance, end) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.distance = distance;
-    this.end = end;
-};
-var time = millis();
-var points = 0;
-var lFrame = false;
-var Button = function(xPosition, yPosition, tWidth, tLength, text) {
-    this.xPosition = xPosition;
-    this.yPosition = yPosition;
-    this.tWidth = tWidth;
-    this.tLength = tLength;
-    this.text = text;
-};
-var start = false;
+  {
+    textFont(loadFont("OCR A", 1));
+    var scene = 0;
+    var rock = loadImage("cute/Rock");
+    var rocks = [];
+    var Rock = function (x, y, size, distance) {
+      this.x = x;
+      this.y = y;
+      this.size = size;
+      this.distance = distance;
+      this.end = false;
+    };
+    var time = millis();
+    var points = 0;
+    var lFrame = false;
+    var Button = function (xPosition, yPosition, tWidth, tLength, text) {
+      this.xPosition = xPosition;
+      this.yPosition = yPosition;
+      this.tWidth = tWidth;
+      this.tLength = tLength;
+      this.text = text;
+    };
+    var start = false;
+  } // variables
 
-Button.prototype.retry = function () {
-    var a;
-    var b;
-    if (mouseX > this.xPosition && mouseX < this.xPosition + this.tWidth && mouseY > this.yPosition && mouseY < this.yPosition + this.tLength) {
+  {
+    Button.prototype.retry = function () {
+      var a;
+      var b;
+      if (
+        mouseX > this.xPosition &&
+        mouseX < this.xPosition + this.tWidth &&
+        mouseY > this.yPosition &&
+        mouseY < this.yPosition + this.tLength
+      ) {
         var a = color(150, 150, 150);
         var b = color(255, 255, 255);
         if (mouseIsPressed) {
-            time = millis();
-            lFrame = false;
-            points = 0;
-            rocks = [];
-            textAlign(LEFT, TOP);
-            textSize(21);
-            start = true;
+          time = millis();
+          points = 0;
+          rocks = [];
+          textAlign(LEFT, TOP);
+          textSize(21);
+          scene = 1;
         }
-    } else {
+      } else {
         var a = color(255, 255, 255);
         var b = color(100, 100, 100);
-
-    }
-    fill(a);
-    rect(this.xPosition, this.yPosition, this.tWidth, this.tLength);
-    fill(b);
-    textAlign(CENTER, CENTER);
-    text("RETRY", this.xPosition + this.tWidth / 2, this.yPosition + this.tLength / 2);
-
-};
-
-var retry = new Button(150, 300, 100, 33, "Retry");
-
-
-    
-
-
-
-textSize(21);
-Rock.prototype.draw = function() {
-    if (this.distance < 100) {
-        image(rock, this.x, this.y, this.size+this.distance, (this.size+this.distance)*1.5);
-        var time = millis();
-        //println(millis() % 429);
-        this.distance += 1;
-            
-        if (mouseX > this.x - (this.size+this.distance) / 2 && mouseX < this.x + (this.size+this.distance) / 2 && mouseY > this.y - (this.size+this.distance) / 2 && mouseY < this.y + (this.size+this.distance) / 2 && mouseIsPressed) {
-            points += 110-this.distance;
-            this.distance = 100;
+      }
+      fill(a);
+      rect(this.xPosition, this.yPosition, this.tWidth, this.tLength);
+      fill(b);
+      textAlign(CENTER, CENTER);
+      text(
+        "RETRY",
+        this.xPosition + this.tWidth / 2,
+        this.yPosition + this.tLength / 2
+      );
+    };
+    Button.prototype.start = function () {
+      Button.prototype.retry = function () {
+        var a;
+        var b;
+        if (
+          mouseX > this.xPosition &&
+          mouseX < this.xPosition + this.tWidth &&
+          mouseY > this.yPosition &&
+          mouseY < this.yPosition + this.tLength
+        ) {
+          var a = color(150, 150, 150);
+          var b = color(255, 255, 255);
+          if (mouseIsPressed) {
+            scene = 1;
+          }
+        } else {
+          var a = color(255, 255, 255);
+          var b = color(100, 100, 100);
         }
-        
-    }
-};
-    
-// draw
-imageMode(CENTER);
-
-draw = function() {
-    var now = millis();
-    if (now - time < 15000) { 
-        background(255, 255, 255);
-        textAlign(LEFT, TOP);
-        // random number, if 1:
-        if (floor(random(1, 45)) === 1) {
-            rocks.push(new Rock(floor(random(50, 350)), floor(random(50, 350)), floor(random(80, 100)), 0));
-        }
-        for (var i = 0; i < rocks.length; i++) {
-            rocks[i].draw();
-        }
-        fill(0, 0, 0);
-        text("Points: " + points + "\nTime: " + ((now- time)/1000).toFixed(3), 5, 5);
-        
-    } else if (!lFrame) {
-        background(255, 255, 255);
-        for (var i = 0; i < rocks.length; i++) {
-            rocks[i].draw();
-        }
-        fill(255, 255, 255, 50);
-        rect(0, 0, 400, 400);
-        fill(0, 0, 0);
-        text("Points: " + points + "\nTime: 15", 5, 5);
-        textSize(41);
+        fill(a);
+        rect(this.xPosition, this.yPosition, this.tWidth, this.tLength);
+        fill(b);
         textAlign(CENTER, CENTER);
-        text("GAME OVER\nYour score is: "+ points, 200, 200);
-        textSize(21);
-        lFrame = true;
-    } else {
-        retry.retry();
-        
+        text(
+          "START",
+          this.xPosition + this.tWidth / 2,
+          this.yPosition + this.tLength / 2
+        );
+      };
+    };
+
+    var retry = new Button(150, 300, 100, 33, "Retry");
+  } // buttons
+
+  noStroke();
+  var stars = [];
+
+  var Star = function (x, y) {
+    this.x = x;
+    this.y = y;
+  };
+
+  Star.prototype.draw = function () {
+    if (floor(random(0, 60)) !== 0) {
+      ellipse(this.x, this.y, 1, 1);
     }
-};
+  };
 
+  for (var i = 0; i < 200; i++) {
+    stars.push(new Star(random(0, 400), random(0, 400)));
+  }
+
+  var drawStars = function () {
+    for (var i = 0; i < stars.length; i++) {
+      stars[i].draw();
+    }
+  };
+
+  textSize(21);
+  Rock.prototype.draw = function () {
+    if (this.distance < 100 && !this.end) {
+      image(
+        rock,
+        this.x,
+        this.y,
+        this.size + this.distance,
+        (this.size + this.distance) * 1.5
+      );
+      var time = millis();
+      //println(millis() % 429);
+      this.distance += 1;
+
+      if (
+        mouseX > this.x - (this.size + this.distance) / 2 &&
+        mouseX < this.x + (this.size + this.distance) / 2 &&
+        mouseY > this.y - (this.size + this.distance) / 2 &&
+        mouseY < this.y + (this.size + this.distance) / 2 &&
+        mouseIsPressed
+      ) {
+        points += 110 - this.distance;
+        this.end = true;
+      }
+    }
+  };
+
+  // draw
+  imageMode(CENTER);
+
+  draw = function () {
+    var now = millis();
+    if (scene === 1) {
+      if (now - time >= 15000) {
+        scene = 2;
+      }
+      background(10, 0, 50);
+      fill(255, 255, 0);
+      drawStars();
+
+      textAlign(LEFT, TOP);
+      // random number, if 1:
+      {
+        if (floor(random(1, 32)) === 1) {
+          rocks.push(
+            new Rock(
+              floor(random(50, 350)),
+              floor(random(50, 350)),
+              floor(random(80, 100)),
+              0
+            )
+          );
+        }
+      } // create rocks
+      {
+        for (var i = 0; i < rocks.length; i++) {
+          rocks[i].draw();
+        }
+      } // draw rocks
+      fill(255, 255, 255);
+      text(
+        "Points: " + points + "\nTime: " + ((now - time) / 1000).toFixed(3),
+        5,
+        5
+      );
+    } else if (scene === 2) {
+      background(10, 0, 50);
+      fill(255, 255, 0);
+      drawStars();
+      for (var i = 0; i < rocks.length; i++) {
+        rocks[i].draw();
+      }
+      fill(255, 255, 255, 50);
+      rect(0, 0, 400, 400);
+      fill(255, 255, 255);
+      text("Points: " + points + "\nTime: 15", 5, 5);
+      textSize(41);
+      textAlign(CENTER, CENTER);
+      text("GAME OVER", 200, 150);
+      textSize(35);
+      text("Your score is:", 200, 200);
+      textSize(57);
+      text(points, 200, 255);
+      textSize(21);
+      scene = 3;
+    } else {
+      retry.retry();
+    }
+  };
 }
-
